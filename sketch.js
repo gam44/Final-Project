efc783d661636e077a180343a605dfe79416284c
gam44 <gam44@duke.edu>
2019-04-25 14:31:22 -0400

    third commit with edits

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var chart = c3.generate({
     x: 'x',
     columns: [ //the 'x' is how I wrote the coordinates on the x axis 
       ['x', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'],
-      ['Prize Money for Women', .43, .4625, .486, .535, .5605, .600, .625, .700, .750, .850, 1, 1.1, 1.15, 1.6, 1.76, 1.88]
+      ['Wimbledon Prize Money for Women', .43, .4625, .486, .535, .5605, .600, .625, .700, .750, .850, 1, 1.1, 1.15, 1.6, 1.76, 1.88]
     ],
       type: 'bar'
   },
@@ -34,7 +34,7 @@ var chart = c3.generate({
 setTimeout(function () { //this makes the men's data come up second
   chart.load({
     columns: [
-      ['Prize Money for Men', .4775, .500, .525, .575, .6025, .630, .655, .70, .75, .85, 1.00, 1.10, 1.15, 1.60, 1.76, 1.88]
+      ['Wimbledon Prize Money for Men', .4775, .500, .525, .575, .6025, .630, .655, .70, .75, .85, 1.00, 1.10, 1.15, 1.60, 1.76, 1.88]
     ]
   })
 }, 1000);
@@ -47,7 +47,7 @@ var chart2 = c3.generate({
     x: 'x',
     columns: [ //the 'x' is how I wrote the coordinates on the x axis 
       ['x', '2015', '2016', '2017', '2018'],
-      ['NWSL Salary Cap', .265, .278, .30, .315]
+      ['National Womens Soccer League (NWSL) Salary Cap', .265, .278, .30, .315]
     ],
       type: 'bar'
   },
@@ -75,7 +75,7 @@ var chart2 = c3.generate({
 setTimeout(function () { //this makes the men's data come up second
   chart2.load({
     columns: [
-      ['MLS Salary Cap', 3.49, 3.66, 3.845, 4.035]
+      ['Major League Soccer (MLS) Salary Cap', 3.49, 3.66, 3.845, 4.035]
     ]
   })
 }, 1000);
@@ -87,7 +87,7 @@ var chart3 = c3.generate({
       x: 'x',
       columns: [ //the 'x' is how I wrote the coordinates on the x axis 
         ['x', '2014', '2015', '2016', '2017', '2018'],
-        ['WNBA Salary Cap', .1075, .1095, .1115, .1135, .1155]
+        ['Womens National Basketball Association (WNBA) Salary Cap', .1075, .1095, .1115, .1135, .1155]
       ],
         type: 'bar'
     },
@@ -114,7 +114,7 @@ var chart3 = c3.generate({
   setTimeout(function () { //this makes the men's data come up second
     chart3.load({
       columns: [
-        ['NBA Salary Cap', 63.065, 70, 94.143999, 99.093000, 99.093000]
+        ['National Basketball Association (NBA) Salary Cap', 63.065, 70, 94.143999, 99.093000, 99.093000]
       ]
     })
   }, 1000);
